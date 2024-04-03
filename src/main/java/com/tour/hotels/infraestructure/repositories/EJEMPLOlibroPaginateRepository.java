a9aaa27d4d8681578333822ff4139864c0a9ea80
edisonnarvaez <edison.narvaez@campusucc.edu.co>
2024-04-03 18:52:22 -0500

    cambios

--- src/main/java/com/tour/hotels/infraestructure/repositories/EJEMPLOlibroPaginateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.firstproject.poo.infraestructure.repositories;
-
-import com.firstproject.poo.infraestructure.entities.Libro;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface libroPaginateRepository extends PagingAndSortingRepository<Libro, Long> {
-}
