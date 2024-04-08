3dcb74dc9e0254af1c54e800c65979684aad61e0
CamiloArturo98 <arthurxx198@gmail.com>
2024-04-07 19:05:07 -0500

    Add table room

--- src/main/java/com/tour/hotels/infraestructure/entities/Room.java
+++ src/main/java/com/tour/hotels/infraestructure/entities/Room.java
@@ -1,4 +1,107 @@
 package com.tour.hotels.infraestructure.entities;
 
+import java.security.Timestamp;
+
+import org.hibernate.mapping.List;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "Room")
+
 public class Room {
+
+@Id
+
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+@Column(name = "id")
+private Long id;
+private int capacity;
+private Long date;
+private Long time;
+private Boolean status;
+private String type;
+private double price;
+@Column (name = "Create_at")
+private Timestamp createdAt;
+@Column (name = "updated_at")
+private Timestamp updatedAt;
+@Column (name = "id_Hotel")
+private Long idHotel;
+
+@ManyToOne
+
+private Hotel hotel;
+
+public Long getId() {
+    return id;
+}
+public void setId(Long id) {
+    this.id = id;
+}
+public int getCapacity() {
+    return capacity;
+}
+public void setCapacity(int capacity) {
+    this.capacity = capacity;
+}
+public Long getDate() {
+    return date;
+}
+public void setDate(Long date) {
+    this.date = date;
+}
+public Long getTime() {
+    return time;
+}
+public void setTime(Long time) {
+    this.time = time;
+}
+public Boolean getStatus() {
+    return status;
+}
+public void setStatus(Boolean status) {
+    this.status = status;
+}
+
+public String getType() {
+    return type;
+}
+public void setType(String type) {
+    this.type = type;
+}
+public double getPrice() {
+    return price;
+}
+public void setPrice(double price) {
+    this.price = price;
+}
+
+public Timestamp getCreatedAt() {
+    return createdAt;
+}
+public void setCreatedAt(Timestamp createdAt) {
+    this.createdAt = createdAt;
+}
+public Timestamp getUpdatedAt() {
+    return updatedAt;
+}
+public void setUpdatedAt(Timestamp updatedAt) {
+    this.updatedAt = updatedAt;
+}
+public long getId_hotel() {
+    return idHotel;
+}
+public void setId_hotel(Long idHotel) {
+    this.idHotel = idHotel;
+}
+
 }
