ee0fd19fe45dc361144d271a67e7e1146a580b58
Edisonnarvaez <124762853+Edisonnarvaez@users.noreply.github.com>
2024-04-10 16:26:31 -0500

    Update HotelDto.java
    dto

--- src/main/java/com/tour/hotels/domain/dto/HotelDto.java
+++ src/main/java/com/tour/hotels/domain/dto/HotelDto.java
@@ -1,5 +1,12 @@
 package com.tour.hotels.domain.dto;
 
 public class HotelDto {
+  private integer hotelId;
+  private String nombre;
+  private String ubicacion;
+  private String descripcion;
+  private String habitacionesDisponibles;
+
+  //getters and setters
 
 }
