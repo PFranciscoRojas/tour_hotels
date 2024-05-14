0cbeba64f397b8f2806380f8e82ba6671c47bb37
Diego Fernando Guerrero <diego.guerrero9612@gmail.com>
2024-05-14 18:38:42 -0500

    Archivos logica hotel

--- src/main/java/com/tour/hotels/infraestructure/HotelRepository.java
+++ src/main/java/com/tour/hotels/infraestructure/HotelRepository.java
@@ -2,6 +2,10 @@ package com.tour.hotels.infraestructure;
 
 import com.tour.hotels.domain.dto.HotelDto;
 import com.tour.hotels.domain.repository.HotelesRepository;
+import com.tour.hotels.infraestructure.entities.Hotel;
+import com.tour.hotels.infraestructure.mapper.HotelMapper;
+import com.tour.hotels.infraestructure.repositories.HotelCroudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,19 +13,29 @@ import java.util.Optional;
 
 @Repository
 public class HotelRepository implements HotelesRepository {
+    @Autowired
+    public HotelCroudRepository hotelRepo;
+    @Autowired
+    public HotelMapper hotelMapper;
     @Override
     public List<HotelDto> getAll() {
-        return null;
+        List<Hotel> resul = (List<Hotel>) hotelRepo.findAll();
+        return hotelMapper.toHotelDto(resul);
     }
+
     @Override
     public Optional<HotelDto> getById(long idHotel) {
-        return Optional.empty();
+        Optional<Hotel> hotel = hotelRepo.findById((int)idHotel);
+        return hotelMapper.toHotelDtoOptional(hotel);
     }
+
     @Override
     public void deleteByID(long idHotel) {}
+
     @Override
     public HotelDto save(HotelDto hotelDto) {
-        return null;
+        Hotel hotel = hotelMapper.toHotel(hotelDto);
+        return hotelMapper.toHotelDto(hotelRepo.save(hotel));
     }
 
 }
