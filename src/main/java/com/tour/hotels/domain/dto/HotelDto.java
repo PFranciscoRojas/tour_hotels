3b2430b2289f4af61b583b1c4ae4528dfed82a70
Diego Fernando Guerrero <diego.guerrero9612@gmail.com>
2024-05-18 16:49:42 -0500

    Cambios Mapper y otros ajustes de Hotel

--- src/main/java/com/tour/hotels/domain/dto/HotelDto.java
+++ src/main/java/com/tour/hotels/domain/dto/HotelDto.java
@@ -1,12 +1,17 @@
 package com.tour.hotels.domain.dto;
 
 import java.security.Timestamp;
+<<<<<<< HEAD
+=======
+import java.text.DateFormat;
+>>>>>>> 347bb2c5377fd802eb48fff636ee57c810b053b7
 
 public class HotelDto {
   private Integer hotelId;
   private String nombre;
   private String ubicacion;
   private String descripcion;
+<<<<<<< HEAD
   private boolean activa;
 
   private Timestamp entrada;
@@ -15,6 +20,13 @@ public class HotelDto {
   private Timestamp fechaRegistro;
 
   private Timestamp fechaSalida;
+=======
+  private String habitacionesDisponibles;
+  private Timestamp Registro;
+  private Timestamp Salida;
+  private Timestamp FechaRegistro;
+  private Timestamp FechaSalida;
+>>>>>>> 347bb2c5377fd802eb48fff636ee57c810b053b7
 
   public Integer getHotelId() {
     return hotelId;
@@ -56,6 +68,7 @@ public class HotelDto {
     this.activa = activa;
   }
 
+<<<<<<< HEAD
   public Timestamp getEntrada() {
     return entrada;
   }
@@ -86,5 +99,37 @@ public class HotelDto {
 
   public void setFechaSalida(Timestamp fechaSalida) {
     this.fechaSalida = fechaSalida;
+=======
+  public Timestamp isRegistro() {
+    return Registro;
+  }
+
+  public void setRegistro(Timestamp registro) {
+    Registro = registro;
+  }
+
+  public Timestamp isSalida() {
+    return Salida;
+  }
+
+  public void setSalida(Timestamp salida) {
+    Salida = salida;
+  }
+
+  public Timestamp getFechaRegistro() {
+    return FechaRegistro;
+  }
+
+  public void setFechaRegistro(Timestamp fechaRegistro) {
+    FechaRegistro = fechaRegistro;
+  }
+
+  public Timestamp getFechaSalida() {
+    return FechaSalida;
+  }
+
+  public void setFechaSalida(Timestamp fechaSalida) {
+    FechaSalida = fechaSalida;
+>>>>>>> 347bb2c5377fd802eb48fff636ee57c810b053b7
   }
 }
