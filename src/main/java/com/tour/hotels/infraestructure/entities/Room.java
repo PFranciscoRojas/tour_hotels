cf12639083a00b50d9e5a9d6ab8947cad691cced
Edisonnarvaez <edison.narvaez@campusucc.edu.co>
2024-05-21 20:11:06 -0500

    ajustes varios format Date y ajustes reservation

--- src/main/java/com/tour/hotels/infraestructure/entities/Room.java
+++ src/main/java/com/tour/hotels/infraestructure/entities/Room.java
@@ -19,100 +19,100 @@ private Boolean status;
 private String type;
 private double price;
 @Column (name = "Create_at")
-private Timestamp createdAt;
+private Date createdAt;
 @Column (name = "updated_at")
-private Timestamp updatedAt;
+private Date updatedAt;
 @Column (name = "id_Hotel")
 private Integer idHotel;
 
 @OneToMany(mappedBy = "room",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 private List<Reservation> reservations;
 
-public Long getId() {
-    return id;
-}
+    public Long getId() {
+        return id;
+    }
 
-public void setId(Long id) {
-    this.id = id;
-}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-public Integer getCapacity() {
-    return capacity;
-}
+    public Integer getCapacity() {
+        return capacity;
+    }
 
-public void setCapacity(Integer capacity) {
-    this.capacity = capacity;
-}
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
 
-public Date getDate() {
-    return date;
-}
+    public Date getDate() {
+        return date;
+    }
 
-public void setDate(Date date) {
-    this.date = date;
-}
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
-public Time getTime() {
-    return time;
-}
+    public Time getTime() {
+        return time;
+    }
 
-public void setTime(Time time) {
-    this.time = time;
-}
+    public void setTime(Time time) {
+        this.time = time;
+    }
 
-public Boolean getStatus() {
-    return status;
-}
+    public Boolean getStatus() {
+        return status;
+    }
 
-public void setStatus(Boolean status) {
-    this.status = status;
-}
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
 
-public String getType() {
-    return type;
-}
+    public String getType() {
+        return type;
+    }
 
-public void setType(String type) {
-    this.type = type;
-}
+    public void setType(String type) {
+        this.type = type;
+    }
 
-public double getPrice() {
-    return price;
-}
+    public double getPrice() {
+        return price;
+    }
 
-public void setPrice(double price) {
-    this.price = price;
-}
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
-public Timestamp getCreatedAt() {
-    return createdAt;
-}
+    public Date getCreatedAt() {
+        return createdAt;
+    }
 
-public void setCreatedAt(Timestamp createdAt) {
-    this.createdAt = createdAt;
-}
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 
-public Timestamp getUpdatedAt() {
-    return updatedAt;
-}
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
 
-public void setUpdatedAt(Timestamp updatedAt) {
-    this.updatedAt = updatedAt;
-}
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 
-public Integer getIdHotel() {
-    return idHotel;
-}
+    public Integer getIdHotel() {
+        return idHotel;
+    }
 
-public void setIdHotel(Integer idHotel) {
-    this.idHotel = idHotel;
-}
+    public void setIdHotel(Integer idHotel) {
+        this.idHotel = idHotel;
+    }
 
-public List<Reservation> getReservations() {
-    return reservations;
-}
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
 
-public void setReservations(List<Reservation> reservations) {
-    this.reservations = reservations;
-}
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
 }
